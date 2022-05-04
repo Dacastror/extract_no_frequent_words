83350c8cec9cb08f2b9c4ab0c435023713e21d1c
Diego Alberto Castro Rodriguez <dacastror@gmail.com>
2022-05-04 01:45:41 -0500

    limpieza del input mejorada

--- js/find_non_frequent_words_in_english.js
+++ js/find_non_frequent_words_in_english.js
@@ -5,7 +5,7 @@ function orderedWords(){
     const text_area = document.getElementById("text_input")
     const input_text = text_area.value
     ordered_words = getWordsByFrequecy(corpus, input_text)
-    console.log(ordered_words)
+    //console.log(ordered_words)
 }
 
 function getWordsByFrequecy(corpus, input_string){
@@ -15,16 +15,20 @@ function getWordsByFrequecy(corpus, input_string){
 }
 
 function cleanInput(string){
-    var new_str = string.toLowerCase()
+    var new_str = string + " "
+    new_str = new_str.toLowerCase()
+    new_str = new_str.replace(/-\n/g, "")
+    new_str = new_str.replace(/\n-/g, " ")
     new_str = new_str.replace(/\n/g, " ")
-    new_str = new_str.replace(/can('|‘|’)t/g, "can not ")
-    new_str = new_str.replace(/i('|‘|’)m/g, " i am ")
-    new_str = new_str.replace(/n('|‘|’)t/g, " not ")
-    new_str = new_str.replace(/('|‘|’)t/g, " not ")
-    new_str = new_str.replace(/('|‘|’)(ll|re|ve|d|s)/g, " ")
-    new_str = new_str.replace(/ - /g," ")
+    new_str = new_str.replace(/can('|‘|’)t[^\p{L}]/gu, "can not ")
+    new_str = new_str.replace(/i('|‘|’)m[^\p{L}]/gu, " i am ")
+    new_str = new_str.replace(/n('|‘|’)t[^\p{L}]/gu, " not ")
+    new_str = new_str.replace(/('|‘|’)t[^\p{L}]/gu, " not ")
+    new_str = new_str.replace(/('|‘|’)(ll|re|ve|d|s)[^\p{L}]/gu, " ")
     new_str = new_str.replace(/[^\p{L} -]/gu, " ") // remover caracteres que no son letras
+    new_str = new_str.replace(/ -/g," ")
     new_str = new_str.replace(/  +/g," ")
+
     new_str = new_str.trim()
     var array = new_str.split(" ")
     array = [...new Set(array)] // remover palabras repetidas
@@ -47,6 +51,9 @@ function sortWords(pairs){
     pairs.sort(function(first, second) {
         return second[1] - first[1];
     });
+
+    console.log(pairs)
+
     sorted_words = []
     for(var i=0; i<pairs.length; i++){
         sorted_words.push(pairs[i][0])
